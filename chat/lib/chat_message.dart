812bc5bc95fd53bd1a08378b589c771f96850a38
Adolfo <adolfo8799@gmail.com>
2023-01-11 21:05:59 -0300

    feat: adds chat_message

---
+++ chat/lib/chat_message.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class ChatMessage extends StatelessWidget {
+  ChatMessage(this.data, this.mine);
+
+  final Map<String, dynamic> data;
+  final bool mine;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+      child: Row(
+        children: [
+          !mine
+              ? Padding(
+                  padding: EdgeInsets.only(right: 16.0),
+                  child: CircleAvatar(
+                    backgroundImage: NetworkImage(data['senderPhotoUrl']),
+                  ),
+                )
+              : Container(),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: mine ? CrossAxisAlignment.end: CrossAxisAlignment.start,
+              children: [
+                data['imgUrl'] != null
+                    ? Image.network(
+                        data['imgUrl'],
+                        width: 250.0,
+                      )
+                    : Text(
+                        data['text'],
+                        textAlign: mine ? TextAlign.end : TextAlign.start,
+                        style: TextStyle(fontSize: 16),
+                      ),
+                Text(
+                  data['senderName'],
+                  style: TextStyle(fontSize: 13, fontWeight: FontWeight.w500),
+                ),
+              ],
+            ),
+          ),
+          mine
+              ? Padding(
+                  padding: EdgeInsets.only(left: 16.0),
+                  child: CircleAvatar(
+                    backgroundImage: NetworkImage(data['senderPhotoUrl']),
+                  ),
+                )
+              : Container(),
+        ],
+      ),
+    );
+  }
+}
--- chat/lib/chat_screen.dart
+++ chat/lib/chat_screen.dart
@@ -1,11 +1,14 @@
 import 'dart:io';
 
 import 'package:chat/text_composer.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:image_picker/image_picker.dart';
 import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
+import 'package:google_sign_in/google_sign_in.dart';
+
+import 'chat_message.dart';
 
 class ChatScreen extends StatefulWidget {
   const ChatScreen({Key? key}) : super(key: key);
@@ -15,11 +18,60 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+  final GoogleSignIn googleSignIn = GoogleSignIn();
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+  User? _currentUser;
 
+  @override
+  void initState() {
+    super.initState();
+    FirebaseAuth.instance.authStateChanges().listen((user) {
+      _currentUser = user;
+    });
+  }
+
+  Future<User?> _getUser() async {
+    if (_currentUser != null) return _currentUser;
+
+    try {
+      final GoogleSignInAccount? googleSignInAccount =
+          await googleSignIn.signIn();
+      final GoogleSignInAuthentication? googleSignInAuthentication =
+          await googleSignInAccount?.authentication;
+
+      final AuthCredential credential = GoogleAuthProvider.credential(
+          idToken: googleSignInAuthentication?.idToken,
+          accessToken: googleSignInAuthentication?.accessToken);
+
+      final UserCredential authResult =
+          await FirebaseAuth.instance.signInWithCredential(credential);
+
+      final User? user = authResult.user;
+
+      return user;
+    } catch (error) {
+      return null;
+    }
+  }
 
   void _sendMessage({String? text, File? img}) async {
+    final user = await _getUser();
+
+    if (user == null) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text("Nao foi possivel fazer o login, tente novamente!"),
+          backgroundColor: Colors.red,
+        ),
+      );
+    }
+
     await Firebase.initializeApp();
-    Map<String, dynamic> data = {};
+    Map<String, dynamic> data = {
+      "uid": user?.uid,
+      "senderName": user?.displayName,
+      "senderPhoto": user?.photoURL
+    };
 
     if (img != null) {
       firebase_storage.UploadTask task = firebase_storage
@@ -32,8 +84,8 @@ class _ChatScreenState extends State<ChatScreen> {
       String url = await taskSnapshot.ref.getDownloadURL();
       data["imgUrl"] = url;
     }
-    
-    if(text != null) data["text"] = text;
+
+    if (text != null) data["text"] = text;
 
     await FirebaseFirestore.instance.collection("messages").add(data);
   }
@@ -41,13 +93,44 @@ class _ChatScreenState extends State<ChatScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldKey,
       appBar: AppBar(
         title: Text(
           "Ola",
         ),
         elevation: 0,
       ),
-      body: TextComposer(_sendMessage),
+      body: Column(
+        children: [
+          Expanded(
+            child: StreamBuilder(
+              stream:
+                  FirebaseFirestore.instance.collection("messages").snapshots(),
+              builder: (BuildContext context,
+                  AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
+                switch (snapshot.connectionState) {
+                  case ConnectionState.none:
+                  case ConnectionState.waiting:
+                    return Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  default:
+                    List<DocumentSnapshot> documents =
+                        snapshot.data!.docs.reversed.toList();
+
+                    return ListView.builder(
+                      itemBuilder: (context, index) {
+                        return ChatMessage(documents[index].data as Map<String, dynamic>, true);
+                      },
+                      itemCount: documents.length,
+                    );
+                }
+              },
+            ),
+          ),
+          TextComposer(_sendMessage)
+        ],
+      ),
     );
   }
 }
